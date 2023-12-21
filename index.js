9fd136053a0ab0d33389a7495474b8d6c2dc952d
Adam Young <adamyoung1414@gmail.com>
2023-12-20 20:11:46 -0500

    added comment messages to index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// create 16 divs inside of grid container
+// create a 16x16 etch a sketch grid
 let gridContainer = document.querySelector('#gridContainer');
 
 for (let i = 0; i < 256; i++) {
@@ -8,6 +8,8 @@ for (let i = 0; i < 256; i++) {
     gridContainer.appendChild(element);
 }
 
+//  add event so when mouse is hovered over a grid item, the background changes from black to white, and vice versa
+// this simulates the etch a sketch logic
 let gridItems = document.querySelectorAll('.gridItem');
 
 gridItems.forEach((gridItem) => {
