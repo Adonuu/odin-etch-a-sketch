ec1b20bc8b85ececa5fe26b5a559ec75719a692d
Adam Young <adamyoung1414@gmail.com>
2023-12-20 19:58:08 -0500

    changed height of container to 90vh added mouseover event to toggle event class black to simulate an etch a sketch

--- index.js
+++ index.js
@@ -4,5 +4,17 @@ let gridContainer = document.querySelector('#gridContainer');
 for (let i = 0; i < 256; i++) {
     const element = document.createElement('div');
     element.setAttribute('id', 'gridItem' + i.toString());
+    element.classList.add('gridItem');
     gridContainer.appendChild(element);
 }
+
+let gridItems = document.querySelectorAll('.gridItem');
+
+console.log(gridItems);
+
+gridItems.forEach((gridItem) => {
+    gridItem.addEventListener('mouseover', (e) => {
+        e.target.classList.toggle('black');
+        console.log('hovered ' + e.target.id);
+    })
+})
